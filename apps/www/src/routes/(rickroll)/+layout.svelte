7c22aad0430239335f8fe43ea9fc3720ccaad3a9
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-10-30 22:17:47 +0700

    feat(www): change rickroll method

--- apps/www/src/routes/(rickroll)/+layout.svelte
+++ apps/www/src/routes/(rickroll)/+layout.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
   <script>
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
   </script>
 </svelte:head>
 
